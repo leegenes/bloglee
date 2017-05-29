e27e1115db8464e3715ecb5d488bc25a9c320510
Lee Haugen <haugenlee@leegenes.local>
2017-05-29 19:20:30 -0400

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.moment import Moment
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+moment = Moment(app)
 
 from app import views, models
